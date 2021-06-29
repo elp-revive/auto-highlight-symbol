51a54a738e2e48798ed8c25ef0094244ad8d4748
JenChieh <jcs090218@gmail.com>
2021-06-29 11:28:14 +0800

    Use run with timer instead idle

--- auto-highlight-symbol.el
+++ auto-highlight-symbol.el
@@ -940,7 +940,7 @@ You can do these operations at One Key!
     (ahs-unhighlight)
     (when (timerp ahs-idle-timer) (cancel-timer ahs-idle-timer))
     (setq ahs-idle-timer
-          (run-with-idle-timer
+          (run-with-timer
            ;; if switch window, immediately change focus/unfocus
            (if (eq ahs-selected-window (selected-window)) 0
              ahs-idle-interval)
